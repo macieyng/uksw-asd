b5473e16b7158c25e7fc829af3164915d42e8f1c
macieyn <maciej.nachtygal@netguru.com>
2022-05-10 22:36:25 +0200

    format document

--- sorting/main.py
+++ sorting/main.py
@@ -1,14 +1,13 @@
 def simple_sort(*args):
     items = list(*args)
     n = len(items)
-    for i in range(0, n-1):
+    for i in range(0, n - 1):
         minimal = i
         for j in range(i + 1, n):
             if items[j] < items[minimal]:
                 minimal = j
         items[i], items[minimal] = items[minimal], items[i]
     return items
-
 
 
 def insert_sort(*args):
@@ -19,18 +18,18 @@ def insert_sort(*args):
         j = i - 1
         while j >= 0 and items[j] > key:
             items[j + 1] = items[j]
-            j = j -1
-        items[j + 1] = key 
+            j = j - 1
+        items[j + 1] = key
     return items
 
 
 def bubble_sort(*args):
     items = list(*args)
-    n = len (items)
+    n = len(items)
     for i in range(n):
-        for j in range(n-1, i, -1):
+        for j in range(n - 1, i, -1):
             if items[j - 1] > items[j]:
-                items[j-1], items[j] = items[j], items[j-1]
+                items[j - 1], items[j] = items[j], items[j - 1]
     return items
 
 
@@ -47,7 +46,7 @@ def quick_sort(*args):
             items[pivot_index], items[i] = items[i], items[pivot_index]
             print(items, pivot_index)
     items[0], items[pivot_index] = items[pivot_index], items[0]
-    return quick_sort(items[:pivot_index+1]) + quick_sort(items[pivot_index+1:])
+    return quick_sort(items[: pivot_index + 1]) + quick_sort(items[pivot_index + 1 :])
 
 
 def merge_sort(*args):
